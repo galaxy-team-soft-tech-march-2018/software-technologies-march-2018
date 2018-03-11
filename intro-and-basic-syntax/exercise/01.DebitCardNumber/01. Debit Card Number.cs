94bd3f2bbbdba07a20deef270cbb39c871e3caca
DimovDimo <d.dimov1996@gmail.com>
2018-03-11 11:56:45 +0200

    added solution of problem 01. Debit Card Number

---
+++ intro-and-basic-syntax/exercise/01.DebitCardNumber/01. Debit Card Number.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.DebitCardNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstDigits = long.Parse(Console.ReadLine());
+            var secondDigits = long.Parse(Console.ReadLine());
+            var thirdDigits = long.Parse(Console.ReadLine());
+            var fourthDigits = long.Parse(Console.ReadLine());
+            Console.WriteLine($"{firstDigits:D4} {secondDigits:D4} {thirdDigits:D4} {fourthDigits:D4}");
+        }
+    }
+}
