fa458891fa88dbf002ea87d26465c5275eaeeddc
mariyast1 <mariyastoykova6@gmail.com>
2018-03-14 12:40:36 +0200

    Adding 03.MilesToKilometers

---
+++ intro-and-basic-syntax/exercise/03.MilesToKilometers/03.MilesToKilometers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.MilesToKilometers
+{
+    class MilesToKilometers
+    {
+        static void Main(string[] args)
+        {
+            var distanceInMiles = double.Parse(Console.ReadLine());
+            const double kilometersPerMile = 1.60934;
+            var distanceInKilometers = distanceInMiles * kilometersPerMile;
+
+            Console.WriteLine("{0:F2}", distanceInKilometers);
+
+        }
+    }
+}
