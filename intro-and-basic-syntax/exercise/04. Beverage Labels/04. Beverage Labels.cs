c86282f67fdba1495fcbe6b2a85385ca873d0a5d
DimovDimo <d.dimov1996@gmail.com>
2018-03-14 11:13:30 +0200

    added 04. Beverage Labels.cs and 05. Character Stats.cs

---
+++ intro-and-basic-syntax/exercise/04. Beverage Labels/04. Beverage Labels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_4.Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameProdukt = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energyContentPer100ml = int.Parse(Console.ReadLine());
+            int sugarContentPer100ml = int.Parse(Console.ReadLine());
+
+            var energy = (volume / 100.0) * energyContentPer100ml;
+            var sugarContent = (volume / 100.0) * sugarContentPer100ml;
+
+            Console.WriteLine($"{volume}ml {nameProdukt}:");
+            Console.WriteLine($"{energy}kcal, {sugarContent}g sugars");
+        }
+    }
+}
