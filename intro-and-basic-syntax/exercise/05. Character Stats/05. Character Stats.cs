0a45b12a9808f90defb3e6871988ddbaa2cd5424
DimovDimo <d.dimov1996@gmail.com>
2018-03-16 16:12:34 +0200

    added comments in 05. Character Stats.cs

--- intro-and-basic-syntax/exercise/05. Character Stats/05. Character Stats.cs
+++ intro-and-basic-syntax/exercise/05. Character Stats/05. Character Stats.cs
@@ -10,13 +10,19 @@ namespace _05.Character_Stats
     {
         static void Main(string[] args)
         {
+            // We reading the information for the Hero
             string nameHero = Console.ReadLine();
             int currentHealth = int.Parse(Console.ReadLine());
             int maximumHealth = int.Parse(Console.ReadLine());
             int currentEnergy = int.Parse(Console.ReadLine());
             int maximumEnergy = int.Parse(Console.ReadLine());
 
+            // We printing the name of the Hero
             Console.WriteLine($"Name: {nameHero}");
+            // We printing the Health and the Energy of the Hero
+            // The Health and the Energy always start and end with '|' therefore |{0}{1}|
+            // They contain so much '|' as current Health or Energy
+            // They contain so much '.' as theirs maximum of Health or Energy subtract by theirs current Health or Energy
             Console.WriteLine("Health: |{0}{1}|", new string('|', currentHealth), new string('.', maximumHealth - currentHealth));
             Console.WriteLine("Energy: |{0}{1}|", new string('|', currentEnergy), new string('.', maximumEnergy - currentEnergy));
         }
