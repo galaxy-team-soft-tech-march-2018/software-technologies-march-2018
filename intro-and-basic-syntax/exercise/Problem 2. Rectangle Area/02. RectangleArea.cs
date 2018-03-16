fde318257d35ddee60e8c6f7ed7e5f7c451925ae
DimovDimo <d.dimov1996@gmail.com>
2018-03-16 15:46:10 +0200

    added comments on 02. RectangleArea.cs

--- intro-and-basic-syntax/exercise/Problem 2. Rectangle Area/02. RectangleArea.cs
+++ intro-and-basic-syntax/exercise/Problem 2. Rectangle Area/02. RectangleArea.cs
@@ -10,11 +10,16 @@ namespace Problem_2.Rectangle_Area
     {
         static void Main(string[] args)
         {
+            // We getting the width of the rectangle
             double width = double.Parse(Console.ReadLine());
+            // We getting the height of the rectangle
             double height = double.Parse(Console.ReadLine());
 
+            // We calculating the area of the rectangle
+            // It's width multiplied by his height
             double area = width * height;
 
+            // We printing the area of the rectangle
             Console.WriteLine($"{area:F2}");
         }
     }
