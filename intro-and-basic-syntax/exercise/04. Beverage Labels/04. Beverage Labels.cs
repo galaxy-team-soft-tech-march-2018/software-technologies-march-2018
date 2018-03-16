b69f332f358eddf1a9b2d33fc80e6ad8f74c5e41
DimovDimo <d.dimov1996@gmail.com>
2018-03-16 15:56:34 +0200

    added comments in 04. Beverage Labels.cs

--- intro-and-basic-syntax/exercise/04. Beverage Labels/04. Beverage Labels.cs
+++ intro-and-basic-syntax/exercise/04. Beverage Labels/04. Beverage Labels.cs
@@ -10,14 +10,20 @@ namespace Problem_4.Beverage_Labels
     {
         static void Main(string[] args)
         {
+            // We reading the information for the product
             string nameProdukt = Console.ReadLine();
             int volume = int.Parse(Console.ReadLine());
             int energyContentPer100ml = int.Parse(Console.ReadLine());
             int sugarContentPer100ml = int.Parse(Console.ReadLine());
 
+            // With (volume / 100.0) we get 1 ml from the product
+            // The energy is 1 ml from the product multiply by energyContentPer100ml
             var energy = (volume / 100.0) * energyContentPer100ml;
+
+            // The sugarContent is 1 ml from the product multiply by sugarContentPer100ml
             var sugarContent = (volume / 100.0) * sugarContentPer100ml;
 
+            // We printing our results
             Console.WriteLine($"{volume}ml {nameProdukt}:");
             Console.WriteLine($"{energy}kcal, {sugarContent}g sugars");
         }
