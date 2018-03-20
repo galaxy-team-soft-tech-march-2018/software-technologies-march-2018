6c09347a2782b8f62ffdbb16cff1f815b7668f48
DimovDimo <d.dimov1996@gmail.com>
2018-03-20 08:20:16 +0200

    change variables names 04. Beverage Labels.cs

--- intro-and-basic-syntax/exercise/04. Beverage Labels/04. Beverage Labels.cs
+++ intro-and-basic-syntax/exercise/04. Beverage Labels/04. Beverage Labels.cs
@@ -11,21 +11,21 @@ namespace Problem_4.Beverage_Labels
         static void Main(string[] args)
         {
             // We reading the information for the product
-            string nameProdukt = Console.ReadLine();
-            int volume = int.Parse(Console.ReadLine());
-            int energyContentPer100ml = int.Parse(Console.ReadLine());
-            int sugarContentPer100ml = int.Parse(Console.ReadLine());
+            string productName = Console.ReadLine();
+            int volumeOfTheProduct = int.Parse(Console.ReadLine());
+            int energyPer100ml = int.Parse(Console.ReadLine());
+            int sugarPer100ml = int.Parse(Console.ReadLine());
 
             // With (volume / 100.0) we get 1 ml from the product
             // The energy is 1 ml from the product multiply by energyContentPer100ml
-            var energy = (volume / 100.0) * energyContentPer100ml;
+            var totalEnergy = (volumeOfTheProduct / 100.0) * energyPer100ml;
 
             // The sugarContent is 1 ml from the product multiply by sugarContentPer100ml
-            var sugarContent = (volume / 100.0) * sugarContentPer100ml;
+            var totalSugar = (volumeOfTheProduct / 100.0) * sugarPer100ml;
 
             // We printing our results
-            Console.WriteLine($"{volume}ml {nameProdukt}:");
-            Console.WriteLine($"{energy}kcal, {sugarContent}g sugars");
+            Console.WriteLine($"{volumeOfTheProduct}ml {productName}:");
+            Console.WriteLine($"{totalEnergy}kcal, {totalSugar}g sugars");
         }
     }
 }
