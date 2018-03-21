cdeaa7e3df9268ae1bfa699a04d5af0c49272184
Georgi Nikolov <gh.nikolov@gmail.com>
2018-03-21 08:22:27 +0200

    Added comments in 01.Debit card number

--- intro-and-basic-syntax/exercise/01.DebitCardNumber/01. Debit Card Number.cs
+++ intro-and-basic-syntax/exercise/01.DebitCardNumber/01. Debit Card Number.cs
@@ -11,14 +11,18 @@ namespace _01.DebitCardNumber
         static void Main(string[] args)
         {
             // Reading the input from the console
-
+            // We read first digit of Debit Card
             var firstDigits = long.Parse(Console.ReadLine());
+            // We read second digit of Debit Card
             var secondDigits = long.Parse(Console.ReadLine());
+            // We read third digit of Debit Card
             var thirdDigits = long.Parse(Console.ReadLine());
+            // We read fourth digit of Debit Card
             var fourthDigits = long.Parse(Console.ReadLine());
 
         // Printing the result on the Console in 4-digit debit card format
 
+            //We print de result
             Console.WriteLine($"{firstDigits:D4} {secondDigits:D4} {thirdDigits:D4} {fourthDigits:D4}");
         }
     }
