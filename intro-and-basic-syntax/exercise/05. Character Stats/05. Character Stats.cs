86ac584f5453e5b0bf372bea0ace1b062ce90679
DimovDimo <d.dimov1996@gmail.com>
2018-03-21 16:31:58 +0200

    changed variables names of problems 2 & 5

--- intro-and-basic-syntax/exercise/05. Character Stats/05. Character Stats.cs
+++ intro-and-basic-syntax/exercise/05. Character Stats/05. Character Stats.cs
@@ -11,20 +11,20 @@ namespace _05.Character_Stats
         static void Main(string[] args)
         {
             // We reading the information for the Hero
-            string nameHero = Console.ReadLine();
-            int currentHealth = int.Parse(Console.ReadLine());
+            string heroName = Console.ReadLine();
+            int health = int.Parse(Console.ReadLine());
             int maximumHealth = int.Parse(Console.ReadLine());
-            int currentEnergy = int.Parse(Console.ReadLine());
+            int energy = int.Parse(Console.ReadLine());
             int maximumEnergy = int.Parse(Console.ReadLine());
 
             // We printing the name of the Hero
-            Console.WriteLine($"Name: {nameHero}");
+            Console.WriteLine($"Name: {heroName}");
             // We printing the Health and the Energy of the Hero
             // The Health and the Energy always start and end with '|' therefore |{0}{1}|
             // They contain so much '|' as current Health or Energy
             // They contain so much '.' as theirs maximum of Health or Energy subtract by theirs current Health or Energy
-            Console.WriteLine("Health: |{0}{1}|", new string('|', currentHealth), new string('.', maximumHealth - currentHealth));
-            Console.WriteLine("Energy: |{0}{1}|", new string('|', currentEnergy), new string('.', maximumEnergy - currentEnergy));
+            Console.WriteLine("Health: |{0}{1}|", new string('|', health), new string('.', maximumHealth - health));
+            Console.WriteLine("Energy: |{0}{1}|", new string('|', energy), new string('.', maximumEnergy - energy));
         }
     }
 }
